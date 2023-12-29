749626e6f90aa484b392e033d09836d4eba24122
shaye <1531688255@qq.com>
2023-12-29 18:38:27 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
-  base: '/',
+  base: '/event/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
